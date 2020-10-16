8be1673ab1cb67a2171df331ebcf6938b8ac2840
Bianca A. Hernandez <bhernan2@gmail.com>
2020-10-16 12:17:36 -0500

    updated

--- page.py
+++ page.py
@@ -13,6 +13,17 @@ import numpy as np
 
 from datetime import datetime
 
+dei_list = ['At work I am treated with respect.', 
+            'At work, I feel comfortable being myself.', 
+            'Diversity and inclusiveness issues are openly discussed.', 
+            'Employees in my organization are treated with respect and dignity.', 
+            'Everyone at this organization is treated fairly regardless of ethnic background, race, gender, age, disability, or other differences not related to job performance.',
+            'I have the same opportunities for advancement as other team members at my organization with similar experience and performance levels.',
+            'If I raised a concern about ethics and integrity, I am confident my employer would do what is right.',
+            'My organization treasures diverse opinions and ideas.',
+            'My team members appreciate my contributions.',
+            'My supervisor tries to understand my point of view.',
+            'My workplace is committed to building the strengths of each employee.']
 
 word_heading = html.H2(children="Let us know what other efforts you'd like to see prioritized", className='lead text-center')
 #word_title
@@ -24,7 +35,8 @@ title = dbc.Jumbotron(
             [
             html.Img(src='assets/foundcom-logo-ffce54.png'),
             html.H1("DEI Survey Dashboard", className='display-3 text-center'),
-            html.H4("Dashboard contributors: DeShan Allison, Bianca A. Hernandez, Michelle Le, Kenya Lewis, Stephanie Perrone, Julie Roebuck, Ebonie Trice, Mary Young and Tiffany Nicely-Williams")
+            html.H4("Dashboard contributors: DeShan Allison, Bianca A. Hernandez, Michelle Le, Kenya Lewis, Stephanie Perrone, Julie Roebuck, Ebonie Trice, Mary Young and Tiffany Nicely-Williams"),
+            html.H4("The Diversity, Equity and Inclusion (DEI) Survey Subcommittee created a survey to gather more information about your experience working at Foundation Communities. Your responses were important to us and helped us learn more about the racial biases, discriminatory practices and unconscious prejudices that affect staff of color and those who belong to marginalized groups within the agency.")
             ]
             ,style={'textAlign': 'center'}
         )
@@ -33,16 +45,24 @@ title = dbc.Jumbotron(
 
 about = dbc.Card([
     dbc.CardBody([
-        #html.H3("About the survey and its process",  className='display-3 text-center'), 
-        dbc.Row([
-            html.H4("The Diversity, Equity and Inclusion Committee created a survey to gather more information about your experience working at Foundation Communities. Your responses were important to us and helped us learn more about the racial biases, discriminatory practices and unconscious prejudices that affect staff of color and those who belong to marginalized groups within the agency. See below to explore survey results.", style={'textAlign': 'center'}),
+            dbc.Row([
+                dbc.Col([
+                    html.H3("About the survey and its process"), 
+                    html.H4("The DEI Survey Subcommitee gathered on __/__/__ to develop an all staff survey. It was relased on __/__/__ and participants had X # of days to complete the survey. The questions selected for the survey included general demographics (race, age, sexual orientation, tenure, etc.) and DEI specific questions (listed on the right). The DEI Survey Subcommitee wanted to answer the following questions:"),
+                    html.H4("1) How does the distribution of demographics (race, age, sexual orientation, tenure and status) compare to the DEI specific responses?"),
+                    html.H4("2) ")
+                    
+            ]),
+                dbc.Col([
+                    html.Ul(id='my-list', children=[html.Li(i) for i in dei_list], className='lead text-left'),
+                ])   
         ])
     ])
-])
+], color= "secondary", inverse=True)
 
 figures_col = dbc.Card([
     dbc.CardBody([
-        html.H3("Explore results", className='display-3 text-left'),
+        #html.H3("Explore results", className='display-3 text-left'),
         dbc.Row([
             dbc.Col([
                 dbc.Container([
@@ -66,7 +86,7 @@ figures_col = dbc.Card([
     ],
 )
 
-                ])
+                ],)
             ]),
             dbc.Col([
                 dbc.Container([
