fd2635bbb02b0516cdf5e791b7af2f238792679f
Bianca A. Hernandez <bhernan2@gmail.com>
2020-10-19 13:03:12 -0500

    updated

--- page.py
+++ page.py
@@ -13,6 +13,8 @@ import numpy as np
 
 from datetime import datetime
 
+from figures import FC_fig1
+
 dei_list = ['At work I am treated with respect.', 
             'At work, I feel comfortable being myself.', 
             'Diversity and inclusiveness issues are openly discussed.', 
@@ -29,13 +31,6 @@ word_heading = html.H2(children="Let us know what other efforts you'd like to se
 #word_title
 #title and survey info 
 
-logo = dbc.Card([
-    dbc.CardBody([
-        dbc.Row([html.Img(src='assets/foundcom-logo-ffce54.png', className='image-center'),
-        ])
-    ])
-])
-
 title = dbc.Jumbotron([
     dbc.Container([
         html.Img(src='assets/foundcom-logo-ffce54.png')
@@ -49,64 +44,54 @@ title = dbc.Jumbotron([
             ])
     ])
 
-about = dbc.Card([
-    dbc.CardBody([
-            dbc.Row([
-                dbc.Col([
-                    html.H3("About the survey and its process"), 
-                    html.H4("The DEI Survey Subcommitee gathered on __/__/__ to develop an all staff survey. It was relased on __/__/__ and participants had X # of days to complete the survey. The questions selected for the survey included general demographics (race, age, sexual orientation, tenure, etc.) and DEI specific questions (listed on the right). The DEI Survey Subcommitee wanted to answer the following questions:"),
-                    html.H4("1) How does the distribution of demographics (race, age, sexual orientation, tenure and status) compare to the DEI specific responses?"),
-                    html.H4("2) ")
-                    
-            ]),
-                dbc.Col([
-                    html.Ul(id='my-list', children=[html.Li(i) for i in dei_list], className= 'mb-10 lead text-left'),
-                ])   
-        ])
-    ])
-], color= "secondary", inverse=True)
-
-
-figures_col = dbc.Card([
-    dbc.CardBody([
-        #html.H3("Explore results", className='display-3 text-left'),
-        dbc.Row([
-            dbc.Col([
-                dbc.Container([
-                    dbc.DropdownMenu( 
-                        label="Explore the results",
-                        bs_size="lg",
-                        className="m-1",
-                        children=[
-                            dbc.DropdownMenuItem("At work I am treated with respect."),
-                            dbc.DropdownMenuItem("At work, I feel comfortable being myself."),
-                            dbc.DropdownMenuItem("Diversity and inclusiveness issues are openly discussed."),
-                            dbc.DropdownMenuItem("Employees in my organization are treated with respect and dignity"),
-                            dbc.DropdownMenuItem("Everyone at this organization is treated fairly regardless of ethnic background, race, gender, age, disability, or other differences not related to job performance."),
-                            dbc.DropdownMenuItem("I have the same opportunities for advancement as other team members at my organization with similar experience and performance levels."),
-                            dbc.DropdownMenuItem("If I raised a concern about ethics and integrity, I am confident my employer would do what is right."),
-                            dbc.DropdownMenuItem("My organization treasures diverse opinions and ideas."),
-                            dbc.DropdownMenuItem("My team members appreciate my contributions."),
-                            dbc.DropdownMenuItem("My supervisor tries to understand my point of view."),
-                            dbc.DropdownMenuItem("My workplace is committed to building the strengths of each employee.")
-
-    ],
-)
-
-                ],)
-            ]),
-            dbc.Col([
-                dbc.Container([
-                    #html.H4('Word cloud is a common technique used for visualizing frequent words in a text where the size of the text represents their frequency. The most frequent words in response to DEI Staff Survey question, "Other (please let us know what other efforts you would like to see prioritized) are shown in the Word Cloud on the left.')
-                ])
-            ])
-        ])
-    ])
+about = dbc.Jumbotron([
+        dbc.Container([
+            html.H3("The subcommittee gathered on __/__/__ to develop an all staff survey. It was relased on __/__/__ and participants had X # of days to complete the survey. The questions selected for the survey included general demographics (race, age, sexual orientation, tenure, etc.) and DEI specific questions ('At work I am treated with respect', 'At work, I feel comfortable being myself', etc.). In addition, there was an open ended question that allowed survey participants to share efforts that they would like to see prioritized. The subcommittee investigated 1) how the distribution of demographics compared to the DEI specific responses, 2) to see if there were any correlations between DEI related question responses and 3) design a wordcloud to address the open ended question mentioned earlier.", className='text-justify'),
+        ]),
+        #dbc.Container([
+            #html.Ul(id='my-list', children=[html.Li(i) for i in dei_list], className= 'mb-10 lead text-left'),  
+            #])
+           
 ])
 
-def FC_logo():
-    heading = logo
-    return heading 
+figures_col = dbc.Jumbotron([
+    html.H4("Explore results", className='text-center'),
+    html.Br(),
+    html.Br(),
+        dbc.Row([
+            dbc.Container([
+                dbc.DropdownMenu( 
+                    label="Compare DEI related questions to demographics",
+                    bs_size="lg",
+                    className="m-2 'text-justify",
+                    children=[
+                        dbc.DropdownMenuItem("At work I am treated with respect."),
+                        dbc.DropdownMenuItem("At work, I feel comfortable being myself."),
+                        dbc.DropdownMenuItem("Diversity and inclusiveness issues are openly discussed."),
+                        dbc.DropdownMenuItem("Employees in my organization are treated with respect and dignity"),
+                        dbc.DropdownMenuItem("Everyone at this organization is treated fairly regardless of ethnic background, race, gender, age, disability, or other differences not related to job performance."),
+                        dbc.DropdownMenuItem("I have the same opportunities for advancement as other team members at my organization with similar experience and performance levels."),
+                        dbc.DropdownMenuItem("If I raised a concern about ethics and integrity, I am confident my employer would do what is right."),
+                        dbc.DropdownMenuItem("My organization treasures diverse opinions and ideas."),
+                        dbc.DropdownMenuItem("My team members appreciate my contributions."),
+                        dbc.DropdownMenuItem("My supervisor tries to understand my point of view."),
+                        dbc.DropdownMenuItem("My workplace is committed to building the strengths of each employee.")
+                        ]
+                    ),
+            dcc.Graph(
+                id='fig1-viz',
+                figure=FC_fig1()
+                ),        
+            ], 'text-align', className='text-left')
+        ])
+    ])
+
+wordcloud = dbc.Jumbotron([
+    dbc.Container([
+                    html.Img(src='assets/final_word_cloud.png', style={'height': '100%','width': '100%'}, className='image-center')
+                ])
+            ])
+                
 
 def DEI_title():
     heading = title
@@ -116,7 +101,11 @@ def about_():
     heading = about
     return heading 
 
-def figures():
+def FC_figures():
     figure = figures_col
     return figure 
+
+def FC_wordcloud():
+    heading = wordcloud
+    return heading 
 
