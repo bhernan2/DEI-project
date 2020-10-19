3c67ff4640d8e930269c0f5d8ccb23fd6aaf6145
Bianca A. Hernandez <bhernan2@gmail.com>
2020-10-19 13:01:57 -0500

    updated

--- dashboard.py
+++ dashboard.py
@@ -4,23 +4,25 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 #from navbar import dash_navbar
-from page import FC_logo, DEI_title, figures, about_
+from page import DEI_title, FC_figures, about_, FC_wordcloud
+
 
 
 #nav = dash_navbar()
 
-dei_logo = FC_logo()
+
 title = DEI_title()
-dei_figures = figures()
+dei_figures = FC_figures()
 dei_about = about_()
+dei_wordcloud = FC_wordcloud()
 
 def Dashboard():
     layout = html.Div([
     #nav, 
-    #dei_logo,
     title,
     dei_about,
-    dei_figures
+    dei_figures,
+    dei_wordcloud
     ])
     return layout 
 #to test if running uncomment below: 
