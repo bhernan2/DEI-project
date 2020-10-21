04100981b51a7e7f2fec7d587709f28da6c06f9b
Bianca A. Hernandez <bhernan2@gmail.com>
2020-10-21 17:12:57 -0500

    added another figure

--- figures.py
+++ figures.py
@@ -91,7 +91,7 @@ def FC_fig1():
             text='At work, I am treated with respect.',
             x=0.5,
             y=0.95,
-            xanchor='right',
+            xanchor='center',
             yanchor= 'top',
             font=dict(
                 size=25,
@@ -116,39 +116,170 @@ def FC_fig1():
         itemdoubleclick="toggle",
         ),  
         updatemenus=list([dict(
-            buttons= list_updatemenus,
-            active=0,
+            buttons= list_updatemenus, 
+            active=3,
             x = 0.2,
             y = -0.2,
             type = 'buttons', 
-            bordercolor = "#3f3f3f",
+            bordercolor = "#000000",
             xanchor='left',
             yanchor='bottom',
-            direction='left')
+            direction='left',
+            pad=dict(
+                l=10,
+                r=10,
+                t=10,
+                b=10)
+                )
         ]),
         barmode='stack',
-        width=1400, 
+        width=1440, 
         height=800,
-        paper_bgcolor='#FFFFFF',
-        plot_bgcolor='#FFFFFF')
+        paper_bgcolor='#cdd3dc',
+        plot_bgcolor='#cdd3dc')
+    
     #defining layout and plotting
     fig = go.Figure(data,layout)
-    #fig.add_layout_image(
-        #dict(
-            #source="/assets/foundcom-logo-ffce54.png",
-            #xref="paper", yref="paper",
-            #x=1, y=1.05,
-            #sizex=0.1, sizey=0.1,
-            #xanchor="right", yanchor="bottom"
-        #)
-    #)
     fig.update_xaxes(showgrid=False, zeroline=False)
     fig.update_yaxes(showgrid=False, zeroline=False)
     return fig
 
 
+def FC_fig2():
+#At work, I feel comfortable being myself.  
+#defining list_updatemenus
+    list_updatemenus = [{'label': 'Total',
+                     'method': 'update',
+                     'args': [{'visible': [True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]}, {'title': 'At work, I feel comfortable being myself.'}]},
+                    {'label': 'Race',
+                     'method': 'update',
+                     'args': [{'visible': [False, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]}, {'title': 'At work, I feel comfortable being myself.'}]},
+                    {'label': 'Age',
+                     'method': 'update',
+                     'args': [{'visible': [False, False, False, False, False, False, False, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False]}, {'title': 'At work, I feel comfortable being myself.'}]},
+                    {'label': 'Sexual Orientation',
+                     'method': 'update',
+                     'args': [{'visible': [False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, False, False, False, False, False, False, False, False, False, False]}, {'title': 'At work, I feel comfortable being myself.'}]},
+                   {'label': 'Tenure',
+                     'method': 'update',
+                     'args': [{'visible': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, False, False, False, False]}, {'title': 'At work, I feel comfortable being myself.'}]},
+                   {'label': 'Status',
+                     'method': 'update',
+                     'args': [{'visible': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True]}, {'title': 'At work, I feel comfortable being myself.'}]}
+                   ]
+#defining graph objects
+    x_total = dei['At work, I feel comfortable being myself.  ']
+    x_white = dei[dei['What is your race? ']=="White"]["At work, I feel comfortable being myself.  "]
+    x_latinx = dei[dei['What is your race? ']=="Latino/Hispanic"]["At work, I feel comfortable being myself.  "]
+    x_black = dei[dei['What is your race? ']=="Black/ African American"]["At work, I feel comfortable being myself.  "]
+    x_two_plus = dei[dei['What is your race? ']=="Two or more Races"]["At work, I feel comfortable being myself.  "]
+    x_asian = dei[dei['What is your race? ']=="Asian"]["At work, I feel comfortable being myself.  "]
+    x_first_peoples = dei[dei['What is your race? ']=="Indian/ Native American"]["At work, I feel comfortable being myself.  "]
+    x_age1 = dei[dei["What is your age?"]=="25-34"]["At work, I feel comfortable being myself.  "]
+    x_age2 = dei[dei["What is your age?"]=="35-44"]["At work, I feel comfortable being myself.  "]
+    x_age3 = dei[dei["What is your age?"]=="45-54"]["At work, I feel comfortable being myself.  "]
+    x_age4 = dei[dei["What is your age?"]=="55-64"]["At work, I feel comfortable being myself.  "]
+    x_age5 = dei[dei["What is your age?"]=="18-24"]["At work, I feel comfortable being myself.  "]
+    x_age6 = dei[dei["What is your age?"]=="65-74"]["At work, I feel comfortable being myself.  "]
+    x_straight = dei[dei["What is your sexual orientation?"]=="Straight"]["At work, I feel comfortable being myself.  "]
+    x_lgbtq = dei[dei["What is your sexual orientation?"]=="LGBTQ+"]["At work, I feel comfortable being myself.  "]
+    x_tenure1 = dei[dei["How long have you worked for FC?"]=="1-3 years"]["At work, I feel comfortable being myself.  "]
+    x_tenure2 = dei[dei["How long have you worked for FC?"]=="3-5 years"]["At work, I feel comfortable being myself.  "]
+    x_tenure3 = dei[dei["How long have you worked for FC?"]=="Less than a year"]["At work, I feel comfortable being myself.  "]
+    x_tenure4 = dei[dei["How long have you worked for FC?"]=="5-10 years"]["At work, I feel comfortable being myself.  "]
+    x_tenure5 = dei[dei["How long have you worked for FC?"]=="10+ years"]["At work, I feel comfortable being myself.  "]
+    x_tenure6 = dei[dei["How long have you worked for FC?"]=="1 year"]["At work, I feel comfortable being myself.  "]
+    x_status1 = dei[dei["What is your employment status?"]=="Full-time"]["At work, I feel comfortable being myself.  "]
+    x_status2 = dei[dei["What is your employment status?"]=="Part-time"]["At work, I feel comfortable being myself.  "]
+    x_status3 = dei[dei["What is your employment status?"]=="Seasonal"]["At work, I feel comfortable being myself.  "]
+    x_status4 = dei[dei["What is your employment status?"]=="Part-time, Seasonal"]["At work, I feel comfortable being myself.  "]
+#defining data
+    data=[
+        go.Histogram(x=x_total,name='   ',opacity = .5),
+        go.Histogram(x=x_white,name='White',opacity = .5),
+        go.Histogram(x=x_latinx,name='Latino/Hispanic',opacity = .5),
+        go.Histogram(x=x_black,name='Black/African American',opacity = .5),
+        go.Histogram(x=x_two_plus,name='Two or more Races',opacity = .5),
+        go.Histogram(x=x_asian,name='Asian',opacity = .5),
+        go.Histogram(x=x_first_peoples,name='Indian/Native American',opacity = .5),
+        go.Histogram(x=x_age1,name='25-34', opacity = .5),
+        go.Histogram(x=x_age2,name='35-44',opacity = .5),
+        go.Histogram(x=x_age3,name='45-54',opacity = .5),
+        go.Histogram(x=x_age4,name='55-64',opacity = .5),
+        go.Histogram(x=x_age5,name='18-24',opacity = .5),
+        go.Histogram(x=x_age6,name='65-74',opacity = .5),
+        go.Histogram(x=x_straight,name='Straight',opacity = .5),
+        go.Histogram(x=x_lgbtq,name='LGBTQ+',opacity = .5),
+        go.Histogram(x=x_tenure1,name='1-3 years',opacity = .5),
+        go.Histogram(x=x_tenure2,name='3-5 years',opacity = .5),
+        go.Histogram(x=x_tenure3,name='< than a year',opacity = .5),
+        go.Histogram(x=x_tenure4,name='5-10 years',opacity = .5),
+        go.Histogram(x=x_tenure5,name='10+ years',opacity = .5),
+        go.Histogram(x=x_tenure6,name='1 year',opacity = .5),
+        go.Histogram(x=x_status1,name='Full-time',opacity = .5),
+        go.Histogram(x=x_status2,name='Part-time',opacity = .5),
+        go.Histogram(x=x_status2,name='Seasonal',opacity = .5),
+        go.Histogram(x=x_status2,name='Part-time, Seasonal',opacity = .5)
+     
+     ]
+#defining layout
+    layout=go.Layout(
+        title=dict(
+            text='At work, I feel comfortable being myself.',
+            x=0.5,
+            y=0.95,
+            xanchor='center',
+            yanchor= 'top',
+            font=dict(
+                size=25,
+                color='#000000'
+            )
 
-
+        ),
+        margin=dict(
+            l=200,
+            r=200,
+            b=150,
+            t=150,
+            pad=4
+        ),
+        font=dict(
+            size=18,
+            color='#000000',
+        ), 
+        legend=dict(
+        # Adjust click behavior
+        itemclick="toggleothers",
+        itemdoubleclick="toggle",
+        ),  
+        updatemenus=list([dict(
+            buttons= list_updatemenus, 
+            active=3,
+            x = 0.2,
+            y = -0.2,
+            type = 'buttons', 
+            bordercolor = "#000000",
+            xanchor='left',
+            yanchor='bottom',
+            direction='left',
+            pad=dict(
+                l=10,
+                r=10,
+                t=10,
+                b=10)
+                )
+        ]),
+        barmode='stack',
+        width=1440, 
+        height=800,
+        paper_bgcolor='#cdd3dc',
+        plot_bgcolor='#cdd3dc')
+    
+    #defining layout and plotting
+    fig = go.Figure(data,layout)
+    fig.update_xaxes(showgrid=False, zeroline=False)
+    fig.update_yaxes(showgrid=False, zeroline=False)
+    return fig
 
 
 
