652efa3f2ee8b1154acd2afc392714576c3ccc8b
Bianca A. Hernandez <bhernan2@gmail.com>
2020-10-26 18:41:13 -0500

    updated

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def Dashboard():
 app = dash.Dash(__name__, external_stylesheets = [dbc.themes.SUPERHERO])
 
 app.layout = Dashboard()
+server = app.server
 if __name__ == "__main__":
     app.run_server(debug=True)
    
