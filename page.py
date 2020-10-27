c69df66b1c7050b581eb6350b12c9a9545352dc6
Bianca A. Hernandez <bhernan2@gmail.com>
2020-10-26 20:52:38 -0500

    updated layout

--- page.py
+++ page.py
@@ -143,9 +143,7 @@ card_content8 = [
 descriptive_stats = html.Div([
             html.Br(),
             dbc.Row([
-                dbc.Col(),
-                html.H3("Interesting Findings", className='display-4 text-center', style={'maxWidth': '1560px'}),
-                dbc.Col(),  
+                html.H3("Interesting Findings", className='display-4 text-center', style={'maxWidth': '1560px'}),  
             ]),
             html.Br(),
             dbc.Row([
