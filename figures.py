db92c1284c11e70d0ea7a027659154bcd74d4d59
Bianca A. Hernandez <bhernan2@gmail.com>
2020-10-27 10:54:25 -0500

    updated

--- figures.py
+++ figures.py
@@ -85,7 +85,7 @@ def FC_fig1():
     #defining layout
     layout=go.Layout(
         title=dict(
-            text='At work, I am treated with respect..',
+            text='At work, I am treated with respect.',
             x=0.5,
             y=0.95,
             xanchor='center',
@@ -963,4 +963,135 @@ def FC_fig7():
     fig.update_yaxes(showgrid=False, zeroline=False)
     return fig 
 
+def FC_fig8():
+    list_updatemenus = [{'label': 'Total',
+                     'method': 'update',
+                     'args': [{'visible': [True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]}, {'title': 'My organization treasures diverse opinions and ideas.'}]},
+                    {'label': 'Race',
+                     'method': 'update',
+                     'args': [{'visible': [False, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]}, {'title': 'My organization treasures diverse opinions and ideas.'}]},
+                    {'label': 'Age',
+                     'method': 'update',
+                     'args': [{'visible': [False, False, False, False, False, False, False, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False]}, {'title': 'My organization treasures diverse opinions and ideas.'}]},
+                    {'label': 'Sexual Orientation',
+                     'method': 'update',
+                     'args': [{'visible': [False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, False, False, False, False, False, False, False, False, False, False]}, {'title': 'My organization treasures diverse opinions and ideas.'}]},
+                   {'label': 'Tenure',
+                     'method': 'update',
+                     'args': [{'visible': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, False, False, False, False]}, {'title': 'My organization treasures diverse opinions and ideas.'}]},
+                   {'label': 'Status',
+                     'method': 'update',
+                     'args': [{'visible': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True]}, {'title': 'My organization treasures diverse opinions and ideas.'}]}
+                   ]
+                   
+    x_total = dei["My organization treasures diverse opinions and ideas."]
+    x_white = dei[dei['What is your race? ']=="White"]["My organization treasures diverse opinions and ideas."]
+    x_latinx = dei[dei['What is your race? ']=="Latino/Hispanic"]["My organization treasures diverse opinions and ideas."]
+    x_black = dei[dei['What is your race? ']=="Black/ African American"]["My organization treasures diverse opinions and ideas."]
+    x_two_plus = dei[dei['What is your race? ']=="Two or more Races"]["My organization treasures diverse opinions and ideas."]
+    x_asian = dei[dei['What is your race? ']=="Asian"]["My organization treasures diverse opinions and ideas."]
+    x_first_peoples = dei[dei['What is your race? ']=="Indian/ Native American"]["My organization treasures diverse opinions and ideas."]
+    x_age1 = dei[dei["What is your age?"]=="25-34"]["My organization treasures diverse opinions and ideas."]
+    x_age2 = dei[dei["What is your age?"]=="35-44"]["My organization treasures diverse opinions and ideas."]
+    x_age3 = dei[dei["What is your age?"]=="45-54"]["My organization treasures diverse opinions and ideas."]
+    x_age4 = dei[dei["What is your age?"]=="55-64"]["My organization treasures diverse opinions and ideas."]
+    x_age5 = dei[dei["What is your age?"]=="18-24"]["My organization treasures diverse opinions and ideas."]
+    x_age6 = dei[dei["What is your age?"]=="65-74"]["My organization treasures diverse opinions and ideas."]
+    x_straight = dei[dei["What is your sexual orientation?"]=="Straight"]["My organization treasures diverse opinions and ideas."]
+    x_lgbtq = dei[dei["What is your sexual orientation?"]=="LGBTQ+"]["My organization treasures diverse opinions and ideas."]
+    x_tenure1 = dei[dei["How long have you worked for FC?"]=="1-3 years"]["My organization treasures diverse opinions and ideas."]
+    x_tenure2 = dei[dei["How long have you worked for FC?"]=="3-5 years"]["My organization treasures diverse opinions and ideas."]
+    x_tenure3 = dei[dei["How long have you worked for FC?"]=="Less than a year"]["My organization treasures diverse opinions and ideas."]
+    x_tenure4 = dei[dei["How long have you worked for FC?"]=="5-10 years"]["My organization treasures diverse opinions and ideas."]
+    x_tenure5 = dei[dei["How long have you worked for FC?"]=="10+ years"]["My organization treasures diverse opinions and ideas."]
+    x_tenure6 = dei[dei["How long have you worked for FC?"]=="1 year"]["My organization treasures diverse opinions and ideas."]
+    x_status1 = dei[dei["What is your employment status?"]=="Full-time"]["My organization treasures diverse opinions and ideas."]
+    x_status2 = dei[dei["What is your employment status?"]=="Part-time"]["My organization treasures diverse opinions and ideas."]
+    x_status3 = dei[dei["What is your employment status?"]=="Seasonal"]["My organization treasures diverse opinions and ideas."]
+    x_status4 = dei[dei["What is your employment status?"]=="Part-time, Seasonal"]["My organization treasures diverse opinions and ideas."] 
+    #defining data
+    data=[
+        go.Histogram(x=x_total,name='   ',opacity = .75),
+        go.Histogram(x=x_white,name='White',opacity = .75),
+        go.Histogram(x=x_latinx,name='Latino/Hispanic',opacity = .75),
+        go.Histogram(x=x_black,name='Black/African American',opacity = .75),
+        go.Histogram(x=x_two_plus,name='Two or more Races',opacity = .75),
+        go.Histogram(x=x_asian,name='Asian',opacity = .5),
+        go.Histogram(x=x_first_peoples,name='Indian/Native American',opacity = .75),
+        go.Histogram(x=x_age1,name='25-34', opacity = .75),
+        go.Histogram(x=x_age2,name='35-44',opacity = .75),
+        go.Histogram(x=x_age3,name='45-54',opacity = .75),
+        go.Histogram(x=x_age4,name='55-64',opacity = .75),
+        go.Histogram(x=x_age5,name='18-24',opacity = .75),
+        go.Histogram(x=x_age6,name='65-74',opacity = .75),
+        go.Histogram(x=x_straight,name='Straight',opacity = .75),
+        go.Histogram(x=x_lgbtq,name='LGBTQ+',opacity = .75),
+        go.Histogram(x=x_tenure1,name='1-3 years',opacity = .75),
+        go.Histogram(x=x_tenure2,name='3-5 years',opacity = .75),
+        go.Histogram(x=x_tenure3,name='< than a year',opacity = .75),
+        go.Histogram(x=x_tenure4,name='5-10 years',opacity = .75),
+        go.Histogram(x=x_tenure5,name='10+ years',opacity = .75),
+        go.Histogram(x=x_tenure6,name='1 year',opacity = .75),
+        go.Histogram(x=x_status1,name='Full-time',opacity = .75),
+        go.Histogram(x=x_status2,name='Part-time',opacity = .75),
+        go.Histogram(x=x_status2,name='Seasonal',opacity = .75),
+        go.Histogram(x=x_status2,name='Part-time, Seasonal',opacity = .75)
+     
+     ]
+    layout=go.Layout(
+        title=dict(
+            text='My organization treasures diverse opinions and ideas.',
+            x=0.5,
+            y=0.95,
+            xanchor='center',
+            yanchor= 'top',
+            font=dict(
+                size=16,
+                color='#000000'
+            )
 
+        ),
+        margin=dict(
+            l=150,
+            r=150,
+            b=75,
+            t=75,
+            pad=4
+        ),
+        font=dict(
+            size=16,
+            color='#000000',
+        ), 
+        legend=dict(
+        # Adjust click behavior
+        itemclick="toggleothers",
+        itemdoubleclick="toggle",
+        ),  
+        updatemenus=list([dict(
+            buttons= list_updatemenus, 
+            active=3,
+            x = 0.1,
+            y = -0.3,
+            type = 'buttons', 
+            bordercolor = "#000000",
+            xanchor='left',
+            yanchor='bottom',
+            direction='left',
+            pad=dict(
+                l=10,
+                r=10,
+                t=10,
+                b=10)
+                )
+        ]),
+        barmode='stack',
+        width=1000, 
+        height=500,
+        paper_bgcolor='#cdd3dc',
+        plot_bgcolor='#cdd3dc')
+
+    #defining layout and plotting
+    fig = go.Figure(data,layout)
+    fig.update_xaxes(showgrid=False, zeroline=False)
+    fig.update_yaxes(showgrid=False, zeroline=False)
+    return fig 
