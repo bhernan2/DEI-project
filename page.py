d5be55951c858206b29d77a64a033da8f109099f
Bianca A. Hernandez <bhernan2@gmail.com>
2020-10-28 10:55:26 -0500

    updated jumbotron

--- page.py
+++ page.py
@@ -36,7 +36,7 @@ title = dbc.Jumbotron([
         html.Img(src='assets/foundcom-logo-ffce54.png')
         ], style={'textAlign': 'center'}),
     dbc.Container([
-            html.H1("FC DEI Survey Dashboard", className='display-3 text-center', style={'maxWidth': '1350px'}),
+            html.H1("FC DEI Survey Dashboard", className='display-3 text-center', style={'maxWidth': '1500px'}),
             html.Br(),
             html.H6("Contributors: DeShan Allison, Bianca A. Hernandez, Michelle Le, Kenya Lewis, Stephanie Perrone, Julie Roebuck, Ebonie Trice, Mary Young and Tiffany Nicely-Williams", className='lead text-center', style={'maxWidth': '1500px'}),
             html.Br(),
@@ -48,17 +48,17 @@ title = dbc.Jumbotron([
             html.H5("The Diversity, Equity and Inclusion (DEI) Survey team created a survey to gather more information about your experience working at Foundation Communities. We researched two surveys: 1) Gallup Diversity and Inclusion and 2) Building Movement Project Race to Lead. We decided to merge questions from both surveys to create the FC DEI Survey. The Gallup survey questions are more focused on individual experience while the Building Movement Project survey is more focused on the racial leadership gap in nonprofit organizations. Both are leaders in collecting data about diversity, equity and inclusion in the workplace. Your responses were important to us and helped us learn more about the racial biases, discriminatory practices and unconscious prejudices that affect staff of color and those who belong to marginalized groups within the agency.", className='text-justify', style={'maxWidth': '1400px'}),                                                                                                                                              
             dbc.Col(width=1),
             ]),
-        ], style={'maxWidth': '1350px'}, fluid=True), 
+        ], style={'maxWidth': '1500px'}, fluid=True), 
     ])     
 
 about = dbc.Jumbotron([
         dbc.Container([
             dbc.Row([
                 dbc.Col(width=1),
-                html.H5("The subcommittee gathered on __/__/__ to develop the all staff survey, it was relased on __/__/__ and participants had X # of days to complete it. Survey participants were asked to rank individual experience questions from 1 to 5 (1 = strongly disagree, 2 = disagree, 3 = neutral, 4 = agree, 5 = strongly agree). The team investigated 1) how the distribution of demographics compared to the individual expierence question responses, 2) to see if there were any correlations between individual experience question responses and 3) designed a wordcloud to analyze an open ended question.", className='text-justify', style={'maxWidth': '1400px'}),  
+                html.H5("The subcommittee gathered on __/__/__ to develop the all staff survey, it was relased on __/__/__ and participants had X # of days to complete it. Survey participants were asked to rank individual experience questions from 1 to 5 (1 = strongly disagree, 2 = disagree, 3 = neutral, 4 = agree, 5 = strongly agree). The team investigated 1) how the distribution of demographics compared to the individual expierence question responses, 2) to see if there were any correlations between individual experience question responses and 3) designed a wordcloud to analyze an open ended question.", className='text-justify', style={'maxWidth': '1500px'}),  
                 dbc.Col(width=1),      
             ]),
-        ], style={'maxWidth': '1350px'}, fluid=True),
+        ], style={'maxWidth': '1500px'}, fluid=True),
         #dbc.Container([
             #html.Ul(id='my-list', children=[html.Li(i) for i in dei_list], className= 'mb-10 lead text-left'),  
             #])
@@ -235,7 +235,7 @@ output_row=html.Div([
                 dbc.Row([
                     dbc.Col(width=1),
                     html.H4('This is where we will add figure summaries focusing on what stood out.'),
-                    dbc.Col(width=1), 
+                    dbc.Col(width=1),
                     ], className="mb-5", style={'maxWidth': '1350px'}),
                 html.Br(),
                 dcc.Graph(
