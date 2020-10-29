1e7f3f23d73efffc619d009497d957f25d13bb0d
Bianca A. Hernandez <bhernan2@gmail.com>
2020-10-29 08:13:49 -0500

    updated

--- figures.py
+++ figures.py
@@ -130,7 +130,7 @@ def FC_fig1():
                 )
         ]),
         barmode='stack',
-        width=900, 
+        width=850, 
         height=400,
         paper_bgcolor='#cdd3dc',
         plot_bgcolor='#cdd3dc')
