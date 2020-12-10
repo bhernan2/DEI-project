768f1f4716fc15324cbf243e005643c5e7bde0d7
Bianca A. Hernandez <bhernan2@gmail.com>
2020-12-10 15:17:59 -0600

    updated for gallup comparison

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import sys
 
 from page import DEI_title, FC_output,  about_, FC_stats, FC_prior, FC_video, FC_wordcloud #FC_dropdown_row, FC_demo_row
@@ -37,6 +37,25 @@ def Dashboard():
 app = dash.Dash(__name__, external_stylesheets = [dbc.themes.SUPERHERO])
 
 app.layout = Dashboard()
+@app.callback(
+    Output("left-collapse", "is_open"),
+    [Input("left", "n_clicks")],
+    [State("left-collapse", "is_open")],
+)
+def toggle_left(n_left, is_open):
+    if n_left:
+        return not is_open
+    return is_open
+@app.callback(
+    Output("left-collapse2", "is_open"),
+    [Input("left2", "n_clicks")],
+    [State("left-collapse2", "is_open")],
+)
+def toggle_left2(n_left, is_open):
+    if n_left:
+        return not is_open
+    return is_open
+
 server = app.server
 if __name__ == "__main__":
     app.run_server(debug=True)
