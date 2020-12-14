38be70913502f07f2140ee065d718d1c6088e1a0
Bianca A. Hernandez <bhernan2@gmail.com>
2020-12-14 12:12:12 -0600

    updated dd, still need to connect 7-11 dd values

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import sys
 
-from page import DEI_title, FC_output,  about_, FC_stats, FC_prior, FC_video, FC_wordcloud, FC_big_pic, FC_dropdown_row #FC_graph1
+from page import DEI_title, about_, FC_stats, FC_prior, FC_video, FC_wordcloud, FC_big_pic, FC_dropdown_row #FC_output
 from figures import FC_fig1, FC_fig2, FC_fig3, FC_fig4, FC_fig5, FC_fig6, FC_fig7, FC_fig8, FC_fig9, FC_fig10, FC_fig11
 
 dei_title = DEI_title()
@@ -15,9 +15,9 @@ dei_about = about_()
 dei_video = FC_video()
 dei_big_pic = FC_big_pic()
 dei_dropdown = FC_dropdown_row()
-dei_output = FC_output()
+#dei_output = FC_output()
 dei_wordcloud = FC_wordcloud()
-#dei_graph1 = FC_graph1()
+
 #this is just to save
 def Dashboard():
     layout = html.Div([
@@ -28,7 +28,7 @@ def Dashboard():
     dei_stats,
     dei_prior,
     dei_wordcloud, 
-    dei_output,
+    #dei_output,
     dei_dropdown,
     #FC_graph1
     ])
@@ -71,55 +71,172 @@ def toggle_left3(n_left, is_open):
 @app.callback(
     dash.dependencies.Output('dd-output-container', 'children'),
     [dash.dependencies.Input('dei-dropdown', 'value')])
-def update_plot1(value):
-    return html.Div([
-        dbc.Container([
-            dbc.Row([
-                dbc.Col([
-                    dcc.Graph(
-                        figure = FC_fig1(),
-                        id='plot1', 
-                        config={
-                            'displayModeBar': False, 
-                            'responsive': True, 
-                            #'autosizable':True,
-                            #'fillFrame':True 
-                            },
-                        style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),                      
-                    ], width={"sm": 12, "md": {"size": 6, "order": 1}, "lg":4},),
-                dbc.Col([
-                    html.P([
-                        html.Ul([
-                            html.Li('The majority of all staff survey respondents agree + strongly agree that they are treated with respect at work and a similar trend is seen across race, age, sexual orientation and tenure.'),
+def update_plot(value):
+    if value == "plot1-info":
+        return html.Div([
+            dbc.Container([
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Graph(
+                            figure = FC_fig1(),
+                            id='plot1', 
+                            config={
+                                'displayModeBar': False, 
+                                'responsive': True, 
+                                #'autosizable':True,
+                                #'fillFrame':True 
+                                },
+                            style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),                      
+                        ], width={"sm": 12, "md": {"size": 6, "order": 1}, "lg":4},),
+                    dbc.Col([
+                        html.P([
+                            html.Ul([
+                                html.Li('The majority of all staff survey respondents agree + strongly agree that they are treated with respect at work and a similar trend is seen across race, age, sexual orientation and tenure.'),
+                            ]),
                         ]),
                     ]),
-                ]),
-            ], align="center"),  
-        ], fluid=True),
-    ])
-    
+                ], align="center"),  
+            ], fluid=True),
+        ])
+    elif value == 'plot2-info':
+        return html.Div([
+            dbc.Container([
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Graph(
+                            figure = FC_fig2(),
+                            id='plot2', 
+                            config={
+                                'displayModeBar': False, 
+                                'responsive': True, 
+                                #'autosizable':True,
+                                #'fillFrame':True 
+                                },
+                            style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),                      
+                        ], width={"sm": 12, "md": {"size": 6, "order": 1}, "lg":4},),
+                    dbc.Col([
+                        html.P([
+                            html.Ul([
+                                html.Li('The majority of all staff survey respondents agree + strongly agree that they feel comforable being themselves at work and a similar trend is seen across race, age, sexual orientation and tenure.'),
+                            ]),
+                        ]),
+                    ]),
+                ], align="center"),  
+            ], fluid=True),
+        ])
+    elif value == 'plot3-info':
+        return html.Div([
+            dbc.Container([
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Graph(
+                            figure = FC_fig3(),
+                            id='plot3', 
+                            config={
+                                'displayModeBar': False, 
+                                'responsive': True, 
+                                #'autosizable':True,
+                                #'fillFrame':True 
+                                },
+                            style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),                      
+                        ], width={"sm": 12, "md": {"size": 6, "order": 1}, "lg":4},),
+                    dbc.Col([
+                        html.P([
+                            html.Ul([
+                                html.Li('65% of all staff survey respondents agree + strongly agree that employees at FC are treated with respect and dignigty.'),
+                                html.Li('However, 25% of staff survey respondents who identify as Black/African American disgaree + strongly disagree with this statement.'),
+                                html.Li('75% of survey respondents within the 18-24 age range agree with this statement.'),
+                                html.Li('Nearly half (46%) of survey respondents in the tenure category of 5-10 years employed are neutral to this statement.')
+                            ]),
+                        ]),
+                    ]),
+                ], align="center"),  
+            ], fluid=True),
+        ])
+    elif value == "plot4-info":
+        return html.Div([
+            dbc.Container([
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Graph(
+                            figure = FC_fig4(),
+                            id='plot4', 
+                            config={
+                                'displayModeBar': False, 
+                                'responsive': True, 
+                                #'autosizable':True,
+                                #'fillFrame':True 
+                                },
+                            style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),                      
+                        ], width={"sm": 12, "md": {"size": 6, "order": 1}, "lg":4},),
+                    dbc.Col([
+                        html.P([
+                            html.Ul([
+                                html.Li('Nearly half of all staff survey respondents agree + strongly agree that diversity and inclusiveness are openly discussed'),
+                                html.Li('83% of survey respondents in the 18-24 age range agree + strongly agree with this statement.'),
+                                html.Li('31% of LGBTQ+ and 23% straight survey respondents disagree + strongly disagree with this statement.'),
+                                html.Li('20% of White and 24% of Latinx/Hispanic survey respondents disagree + strongly disagree with this statement.')
+                            ]),
+                        ]),
+                    ]),
+                ], align="center"),  
+            ], fluid=True),
+        ])
 
-    
-
-
-
-# @app.callback(Output('plot1', 'figure'),
-#               Input('dei-dropdown', 'value'))
-# def update_graph_a(value):
-#     # prepare figure_a from value
-#     return FC_fig1
-
-# @app.callback(Output('plot2', 'figure'),
-#               Input('dei-dropdown', 'value'))
-# def update_graph_b(value):
-#     # prepare figure_b from value
-#     return FC_fig2
-
-# @app.callback(Output('plot3', 'figure'),
-#               Input('dei-dropdown', 'value'))
-# def update_graph_c(value):
-#     # prepare figure_c from value
-#     return FC_fig4
+    elif value == "plot5-info":
+        return html.Div([
+            dbc.Container([
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Graph(
+                            figure = FC_fig5(),
+                            id='plot5', 
+                            config={
+                                'displayModeBar': False, 
+                                'responsive': True, 
+                                #'autosizable':True,
+                                #'fillFrame':True 
+                                },
+                            style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),                      
+                        ], width={"sm": 12, "md": {"size": 6, "order": 1}, "lg":4},),
+                    dbc.Col([
+                        html.P([
+                            html.Ul([
+                                html.Li('Survey respondents in the 18-24 age range agree + strongly agree (83%) that everyone at this organization is treated fairy regardless of ethnic background, race, gender, age, disability, or other differences not related to job performance.'),
+                                html.Li('71% of survey respondents who have worked af FC less than one year agree + strongly agree with this statement'),
+                            ]),
+                        ]),
+                    ]),
+                ], align="center"),  
+            ], fluid=True),
+        ])
+    elif value == "plot6-info":
+        return html.Div([
+            dbc.Container([
+                dbc.Row([
+                    dbc.Col([
+                        dcc.Graph(
+                            figure = FC_fig6(),
+                            id='plot6', 
+                            config={
+                                'displayModeBar': False, 
+                                'responsive': True, 
+                                #'autosizable':True,
+                                #'fillFrame':True 
+                                },
+                            style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),                      
+                        ], width={"sm": 12, "md": {"size": 6, "order": 1}, "lg":4},),
+                    dbc.Col([
+                        html.P([
+                            html.Ul([
+                                html.Li('58%, 22% and 19% of all staff survey respndents either agree + strongly agree, are neutral, or disagree + strongly disgree that they have the same opportunities as other team members at FC with similar experience and performance levels, respectively.'),
+                                html.Li('20%, 25%, 20% and 19% of employees who have worked at FC for one year, 1-3 years, 3-5 years and 5-10 years disagree + strongly disagree with this statement'),
+                            ]),
+                        ]),
+                    ]),
+                ], align="center"),  
+            ], fluid=True),
+        ])
 
 server = app.server
 if __name__ == "__main__":
