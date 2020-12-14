7f4eadc3dccefcf40d272eafcd7e28cf86ad9637
Bianca A. Hernandez <bhernan2@gmail.com>
2020-12-14 10:32:46 -0600

    added callback for dcc.Dropdown, still working on connecting other figures to dd values

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import sys
 
-from page import DEI_title, FC_output,  about_, FC_stats, FC_prior, FC_video, FC_wordcloud, FC_big_pic #FC_dropdown_row, FC_demo_row
-
+from page import DEI_title, FC_output,  about_, FC_stats, FC_prior, FC_video, FC_wordcloud, FC_big_pic, FC_dropdown_row #FC_graph1
+from figures import FC_fig1, FC_fig2, FC_fig3, FC_fig4, FC_fig5, FC_fig6, FC_fig7, FC_fig8, FC_fig9, FC_fig10, FC_fig11
 
 dei_title = DEI_title()
 dei_stats = FC_stats()
@@ -14,10 +14,10 @@ dei_prior=FC_prior()
 dei_about = about_()
 dei_video = FC_video()
 dei_big_pic = FC_big_pic()
-#dei_demo = FC_demo_row()
-#dei_dropdown = FC_dropdown_row()
+dei_dropdown = FC_dropdown_row()
 dei_output = FC_output()
 dei_wordcloud = FC_wordcloud()
+#dei_graph1 = FC_graph1()
 #this is just to save
 def Dashboard():
     layout = html.Div([
@@ -28,9 +28,9 @@ def Dashboard():
     dei_stats,
     dei_prior,
     dei_wordcloud, 
-    dei_output
-    #dei_demo,
-    #dei_dropdown,
+    dei_output,
+    dei_dropdown,
+    #FC_graph1
     ])
     return layout 
 
@@ -67,6 +67,59 @@ def toggle_left3(n_left, is_open):
     if n_left:
         return not is_open
     return is_open
+
+@app.callback(
+    dash.dependencies.Output('dd-output-container', 'children'),
+    [dash.dependencies.Input('dei-dropdown', 'value')])
+def update_plot1(value):
+    return html.Div([
+        dbc.Container([
+            dbc.Row([
+                dbc.Col([
+                    dcc.Graph(
+                        figure = FC_fig1(),
+                        id='plot1', 
+                        config={
+                            'displayModeBar': False, 
+                            'responsive': True, 
+                            #'autosizable':True,
+                            #'fillFrame':True 
+                            },
+                        style={'width': '49%', 'display': 'inline-block', 'vertical-align': 'middle'}),                      
+                    ], width={"sm": 12, "md": {"size": 6, "order": 1}, "lg":4},),
+                dbc.Col([
+                    html.P([
+                        html.Ul([
+                            html.Li('The majority of all staff survey respondents agree + strongly agree that they are treated with respect at work and a similar trend is seen across race, age, sexual orientation and tenure.'),
+                        ]),
+                    ]),
+                ]),
+            ], align="center"),  
+        ], fluid=True),
+    ])
+    
+
+    
+
+
+
+# @app.callback(Output('plot1', 'figure'),
+#               Input('dei-dropdown', 'value'))
+# def update_graph_a(value):
+#     # prepare figure_a from value
+#     return FC_fig1
+
+# @app.callback(Output('plot2', 'figure'),
+#               Input('dei-dropdown', 'value'))
+# def update_graph_b(value):
+#     # prepare figure_b from value
+#     return FC_fig2
+
+# @app.callback(Output('plot3', 'figure'),
+#               Input('dei-dropdown', 'value'))
+# def update_graph_c(value):
+#     # prepare figure_c from value
+#     return FC_fig4
 
 server = app.server
 if __name__ == "__main__":
